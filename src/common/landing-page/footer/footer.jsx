e42b1b1bcdef33f9e901b621c81ca75d690da74f
shootup <vamsikrishna@shootup.in>
2023-03-20 18:35:13 +0530

    added footer

--- src/common/landing-page/footer/footer.jsx
+++ src/common/landing-page/footer/footer.jsx
@@ -1,47 +1,149 @@
-import FacebookIcon from '@/assets/icons/landing-page/facebook'
+import * as React from "react";
+import { motion, useMotionValue, useTransform } from "framer-motion";
+import FacebookIcon from "@/assets/icons/landing-page/facebook";
 import Instagram from '@/assets/icons/landing-page/instagram'
-import Mesh2 from '@/assets/icons/landing-page/mesh2'
 import Logo2 from '@/assets/icons/landing-page/logo2'
+import Mesh2 from '@/assets/icons/landing-page/mesh2'
 import Twitter from '@/assets/icons/landing-page/twitter'
 import YoutubeIcon from '@/assets/icons/landing-page/youtube'
-import GetStartedButton from '@/element/landing-page/button/get-started-button'
+import GetStartedButton from "@/element/landing-page/button/get-started-button";
+import BoxTheme from "@/element/landing-page/box-theme/box-theme";
+import { TextField, Avatar, AvatarGroup, Button } from '@mui/material'
+import SubscribeSubmitButton from "@/element/landing-page/button/subscribe-submit-button";
+import Arrow from '@/assets/icons/landing-page/arrow'
+import Star4 from "@/assets/icons/landing-page/star4";
 
-export default function Footer() {
+export default function Footer({className}) {
+  const currentYear = new Date().getFullYear();
+  const x = useMotionValue(0);
+  const [dragDirection, setDragDirection] = React.useState("");
+
+  const handleDrag = (event, info) => {
+    if (info.offset.x > 0) {
+      setDragDirection("right");
+    } else if (info.offset.x < 0) {
+      setDragDirection("left");
+    }
+  };
   return (
-    <div className='w-[95vw] mx-auto border-2 relative border-primary-brown h-[488px] rounded-[30px]'>
-       <div className="overflow-hidden absolute top-0 w-full h-full -z-1 bg-white ">
-        <Mesh2 className={`scale-[1.2] opacity-[0.1] `}/>
+    <div className='w-[95vw] mx-auto border-2 relative border-primary-brown h-[488px] rounded-[30px] pt-10 mt-96'>
+      <div className='flex justify-center -mt-96 mb-[350px]'>
+      <BoxTheme
+    className={`h-[60px] ${className} w-fit  text-2xl font-medium`}
+    boxStyle={
+      "w-[1100px] h-[400px] flex flex-col border bg-[#b6eccc] rounded-[8px] border-primary-brown"
+    }
+    bgBoxStyle={
+      "w-[1100px] h-[400px]  rounded-[8px]  bg-primary-brown top-[5px] left-[4px]"
+    }
+  >
+    <Star4 className={`absolute -top-8 -left-8 scale-[0.] ${
+          dragDirection === "left" ? "block" : "hidden"
+        } fill-custom-yellow animate-spin`}/>
+        <Star4 className={`absolute -top-8 -right-8 scale-[0.] ${
+          dragDirection === "right" ? "block" : "hidden"
+        } fill-custom-blue animate-spin`}/>
+    <div className='flex w-full justify-between'>
+      <div className="flex flex-col w-[30%] gap-y-3 items-start ml-10">
+      <AvatarGroup max={4}>
+      <Avatar className={`${dragDirection === "left" ? "animate-bounce" : ""}`} alt="Remy" src="/static/images/avatar/1.jpg"/>
+      <Avatar className={`${dragDirection === "left" ? "animate-bounce" : ""}`} alt="Travis" src="/static/images/avatar/2.jpg" />
+      <Avatar className={`${dragDirection === "left" ? "animate-bounce" : ""}`} alt="Cindy" src="/static/images/avatar/3.jpg" />
+    </AvatarGroup>
+      <h2>Job Seekers</h2>
+      <p className='text-lg'>Lorem ipsum dolor sit alam, consectetur adipiscing</p>
+      </div>
+      <div className="flex flex-col w-[30%] gap-y-3 items-end mr-10">
+      <AvatarGroup max={4}>
+      <Avatar className={`${dragDirection === "right" ? "animate-bounce" : ""}`} alt="Remy" src="/static/images/avatar/1.jpg" />
+      <Avatar className={`${dragDirection === "right" ? "animate-bounce" : ""}`} alt="Travis" src="/static/images/avatar/2.jpg" />
+      <Avatar className={`${dragDirection === "right" ? "animate-bounce" : ""}`} alt="Cindy" src="/static/images/avatar/3.jpg" />
+    </AvatarGroup>
+      <h2>Employers</h2>
+      <p className='text-lg text-right'>Lorem ipsum dolor sit alam, consectetur adipiscing</p>
+      </div>
+    </div>
+    <div className='flex w-[93%] justify-center bg-white rounded-[50px] mt-28 border-2 border-primary-brown'>
+      <Arrow className="rotate-180 self-center"/>
+      <h2 className='text-lg text-gray-400 self-center mt-2 ml-5'>I Need Job</h2>
+      <motion.div onDrag={handleDrag} className="box" style={{x}} drag="x" dragConstraints={{left:0, right:0}}>
+      <Button className='bg-secondary-orange hover:bg-secondary-orange capitalize h-10 w-24 border-2 border-solid border-t-[1px] border-b-[1px] rounded-[50px] border-primary-brown ml-5 mr-5'>
+        Sign up
+      </Button>
+      </motion.div>
+      <h2 className='text-lg self-center text-gray-400 mt-2 mr-5'>I Need Candidates</h2>
+      <Arrow className="self-center"/>
+    </div>
+      </BoxTheme>
+      </div>
+       <div className="overflow-hidden flex gap-x-[166px] absolute top-0 w-full h-full -z-1 bg-white ">
+        <Mesh2 className={`scale-[1.2] opacity-[0.1]`}/>
+        <Mesh2 className={`scale-[1.2] opacity-[0.1]`}/>
       </div>
 
-      <div className='flex items-center gap-8'>
+      <div className='flex items-center gap-52 justify-center'>
             <div className='basis-[300px]' >
                 <Logo2/>
                 <p className='text-2xl mt-5 mb-10'>Meet the new gold standard in remote Hyring</p>
                 <GetStartedButton
                 boxColor={"bg-primary-orange"}/>
             </div>
-            <div>
+            <div className='flex flex-col gap-y-5 mb-16'>
                 <h2 className='text-[22px] font-medium'>Company</h2>
                 <p className='text-lg'>Why India</p>
                 <p className='text-lg'>Pricing</p>
                 <p className='text-lg'>Contact Us</p>
             </div>
-            <div>
+            <div className='flex flex-col gap-y-5 mb-16'>
             <h2 className='text-[22px] font-medium'>Legal</h2>
                 <p className='text-lg'>Privacy Policy</p>
                 <p className='text-lg'>Terms & Conditions</p>
                 <p className='text-lg'>Cancellation Policy</p>
             </div>
-            <div>
+            <div className='flex flex-col gap-y-16 mt-7'>
+            <div className='flex flex-col gap-y-5'>
             <h2 className='text-[22px] font-medium'>Social</h2>
             <div className='flex items-center gap-5'>
+              <a href='https://in.linkedin.com/company/hyring-com' target='_blank'>
                 <Instagram/>
+              </a>
                 <YoutubeIcon/>
+                <a href='https://www.facebook.com/HyringHQ/' target='_blank'>
                 <FacebookIcon/>
+                </a>
                 <Twitter/>
             </div>
 
             </div>
+            <div className='flex flex-col gap-y-5'>
+            <h2 className='text-[22px] font-medium'>Subscribe Newsletter</h2>
+            <div className='flex items-center gap-5'>
+            <BoxTheme
+    className={`h-[60px] ${className} w-fit cursor-pointer  text-2xl font-medium`}
+    boxStyle={
+      "w-[270px] h-[57px] border bg-white rounded-[8px] border-primary-brown"
+    }
+    bgBoxStyle={
+      "w-[270px] h-[57px]  rounded-[8px]  bg-primary-brown top-[5px] left-[4px]"
+    }
+  >
+    <TextField
+    className='w-[270px]'
+  hiddenLabel
+  id="filled-hidden-label-normal"
+  placeholder='Your Email Address'
+/>
+
+    
+  </BoxTheme>
+  <SubscribeSubmitButton/>
+            </div>
+
+            </div>
+            </div>
+      </div>
+      <div className='border-t-[1px] border-[#593a25] justify-center flex pt-5 border-solid mt-16 ml-24 mr-24'>
+      Copyright © {currentYear} Hyring. All rights reserved.
       </div>
     </div>
   )

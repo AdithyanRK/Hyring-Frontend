4f4b8b4abaf823f4a80b28a016713a1624197c66
shootup <vamsikrishna@shootup.in>
2023-03-23 16:31:32 +0530

    font fixed in footer

--- src/common/landing-page/footer/footer.jsx
+++ src/common/landing-page/footer/footer.jsx
@@ -27,7 +27,7 @@ export default function Footer({className}) {
     }
   };
   return (
-    <div className='footer w-[95vw] mx-auto border-2 relative border-primary-brown xl:h-[488px] h-[700px] rounded-[30px] pt-10 mt-80'>
+    <div className='footer font-primaryMedium w-[95vw] mx-auto border-2 relative border-primary-brown xl:h-[488px] h-[700px] rounded-[30px] pt-10 mt-80'>
       <div className='green-mar-sec flex justify-center -mt-[280px] mb-[250px]'>
       <BoxTheme
     className={`h-[60px] ${className} w-fit  text-2xl  `}
@@ -52,7 +52,7 @@ export default function Footer({className}) {
       <Avatar className={`${dragDirection === "left" ? "avatar" : ""}`} alt="Cindy" src="/static/images/avatar/3.jpg" />
     </AvatarGroup>
       <h2>Job Seekers</h2>
-      <p className='text-lg'>Lorem ipsum dolor sit alam, consectetur adipiscing</p>
+      <p className='text-lg font-primary'>Lorem ipsum dolor sit alam, consectetur adipiscing</p>
       </div>
       <div className={`${dragDirection === "left" ? " opacity-25" : ""} flex flex-col w-[30%] gap-y-3 items-end mr-10`}>
       <AvatarGroup max={4}>
@@ -61,18 +61,18 @@ export default function Footer({className}) {
       <Avatar className={`${dragDirection === "right" ? "avatar" : ""}`} alt="Cindy" src="/static/images/avatar/3.jpg" />
     </AvatarGroup>
       <h2>Employers</h2>
-      <p className='text-lg text-right'>Lorem ipsum dolor sit alam, consectetur adipiscing</p>
+      <p className='text-lg text-right font-primary'>Lorem ipsum dolor sit alam, consectetur adipiscing</p>
       </div>
     </div>
     <motion.div ref={constraintsRef} className='drag-area flex w-[93%] justify-center bg-white rounded-[50px] mt-7 mb-5 border-2 border-primary-brown'>
       <Arrow className="arrow-left self-center ml-10"/>
-      <h2 className='job-title text-lg text-gray-400 self-center ml-5 mr-5'>I Need Job</h2>
+      <h2 className='job-title text-lg text-gray-400 self-center ml-5 mr-5 font-primary'>I Need Job</h2>
       <motion.div drag="x" dragElastic={0} onDrag={handleDrag} className="box" style={{x}} dragConstraints={constraintsRef}>
-      <Button className='bg-secondary-orange hover:bg-secondary-orange drag-button m-0 capitalize h-10 w-24 border-2 border-solid border-t-[1px] border-b-[1px] rounded-[50px] border-primary-brown ml-5 mr-5'>
+      <Button className='bg-secondary-orange font-primaryBold hover:bg-secondary-orange drag-button m-0 capitalize h-10 w-24 border-2 border-solid border-t-[1px] border-b-[1px] rounded-[50px] border-primary-brown ml-5 mr-5'>
         Sign up
       </Button>
       </motion.div>
-      <h2 className='job-title text-lg self-center text-gray-400 mr-5 ml-5'>I Need Candidates</h2>
+      <h2 className='job-title text-lg self-center text-gray-400 mr-5 ml-5 font-primary'>I Need Candidates</h2>
       <Arrow className=" arrow-right self-center"/>
     </motion.div>
       </BoxTheme>
@@ -95,18 +95,18 @@ export default function Footer({className}) {
                 boxColor={"bg-primary-orange"}/>
             </div>
             <div className='company-sec flex flex-col gap-y-5 mb-16'>
-                <h2 className='text-[22px]  '>Company</h2>
-                <p className='text-lg'>Why India</p>
-                <p className='text-lg'>Pricing</p>
+                <h2 className='text-[22px]'>Company</h2>
+                <p className='text-lg font-primary'>Why India</p>
+                <p className='text-lg font-primary'>Pricing</p>
                 <a href="/contact-us">
-                <p className='text-lg'>Contact Us</p>
+                <p className='text-lg font-primary'>Contact Us</p>
                 </a>
             </div>
             <div className='company-sec flex flex-col gap-y-5 mb-16'>
             <h2 className='text-[22px]  '>Legal</h2>
-                <p className='text-lg'>Privacy Policy</p>
-                <p className='text-lg'>Terms & Conditions</p>
-                <p className='text-lg'>Cancellation Policy</p>
+                <p className='text-lg font-primary'>Privacy Policy</p>
+                <p className='text-lg font-primary'>Terms & Conditions</p>
+                <p className='text-lg font-primary'>Cancellation Policy</p>
             </div>
             <div className='desk-soc flex-col gap-y-12 mt-7'>
             <div className='flex flex-col gap-y-7'>
@@ -191,7 +191,7 @@ export default function Footer({className}) {
 
             </div>
             </div>
-      <div className='copy-sec border-t-[1px] border-[#593a25] justify-center flex pt-3 border-solid xl:mt-16 mt-10 ml-24 mr-24'>
+      <div className='copy-sec border-t-[1px] font-primary border-[#593a25] justify-center flex pt-3 border-solid xl:mt-16 mt-10 ml-24 mr-24'>
       Copyright © {currentYear} Hyring. All rights reserved.
       </div>
     </div>
